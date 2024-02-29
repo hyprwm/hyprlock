4aeea1d5809061bd0670d59afb0225b7070a7d96
Mihai Fufezan <fufexan@protonmail.com>
2024-02-29 15:13:35 +0200

    Nix: refactor flake

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
 
     hyprlang = {
       url = "github:hyprwm/hyprlang";
@@ -10,33 +11,34 @@
     };
   };
 
-  outputs = inputs: let
-    inherit (inputs.nixpkgs) lib;
-    genSystems = lib.genAttrs [
-      # Add more systems if they are supported
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    pkgsFor = genSystems (system:
-      import inputs.nixpkgs {
-        overlays = [inputs.self.overlays.default];
-        inherit system;
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+    ...
+  } @ inputs: let
+    inherit (nixpkgs) lib;
+    eachSystem = lib.genAttrs (import systems);
+    pkgsFor = eachSystem (system:
+      import nixpkgs {
+        localSystem.system = system;
+        overlays = with self.overlays; [default];
       });
   in {
     overlays = import ./nix/overlays.nix {inherit inputs lib;};
 
-    packages = genSystems (system: {
+    packages = eachSystem (system: {
+      default = self.packages.${system}.hyprlock;
       inherit (pkgsFor.${system}) hyprlock;
-      default = inputs.self.packages.${system}.hyprlock;
     });
 
     homeManagerModules = {
-      hyprlock = import ./nix/hm-module.nix inputs.self;
-      default = inputs.self.homeManagerModules.hyprlock;
+      default = self.homeManagerModules.hyprlock;
+      hyprlock = import ./nix/hm-module.nix self;
     };
 
-    checks = genSystems (system: inputs.self.packages.${system});
+    checks = eachSystem (system: self.packages.${system});
 
-    formatter = genSystems (system: pkgsFor.${system}.alejandra);
+    formatter = eachSystem (system: pkgsFor.${system}.alejandra);
   };
 }
--- nix/overlays.nix
+++ nix/overlays.nix
@@ -8,7 +8,9 @@
     (builtins.substring 6 2 longDate)
   ]);
 in {
-  default = lib.composeManyExtensions [
+  default = inputs.self.overlays.hyprlock;
+
+  hyprlock = lib.composeManyExtensions [
     inputs.hyprlang.overlays.default
     (final: prev: {
       hyprlock = prev.callPackage ./default.nix {
