4f381bf3cb5c095fd3711f4dbcf6870044071024
Abi Hafshin Alfarouq <abi@hafs.in>
2024-03-09 15:08:33 +0000

    Handle slow pam verification
    For example when using systemd-homed with fido auth

--- src/core/hyprlock.cpp
+++ src/core/hyprlock.cpp
@@ -371,6 +371,10 @@ void CHyprlock::run() {
             int ret = poll(pollfds, 1, 5000 /* 5 seconds, reasonable. Just in case we need to terminate and the signal fails */);
 
             if (ret < 0) {
+                if (errno == EINTR) {
+                    continue;
+                }
+
                 Debug::log(CRIT, "[core] Polling fds failed with {}", errno);
                 attemptRestoreOnDeath();
                 m_bTerminate = true;
