1f15da34d01fef7c4820548647c8d717b2d4e802
Maximilian Seidler <maximilian.seidler@soundwork.at>
2024-03-10 15:11:13 +0100

    misc: use currentDir as the argument to absolutePath

--- src/helpers/MiscFunctions.cpp
+++ src/helpers/MiscFunctions.cpp
@@ -1,7 +1,7 @@
 #include <filesystem>
 #include "MiscFunctions.hpp"
 
-std::string absolutePath(const std::string& rawpath, const std::string& rawcurrentpath) {
+std::string absolutePath(const std::string& rawpath, const std::string& currentDir) {
     std::filesystem::path path(rawpath);
 
     // Handling where rawpath starts with '~'
@@ -11,8 +11,7 @@ std::string absolutePath(const std::string& rawpath, const std::string& rawcurre
     }
     // Handling e.g. ./, ../
     else if (path.is_relative()) {
-        const std::filesystem::path currentDir = std::filesystem::path(rawcurrentpath).parent_path();
-        return std::filesystem::weakly_canonical(currentDir / path);
+        return std::filesystem::weakly_canonical(std::filesystem::path(currentDir) / path);
     } else {
         return std::filesystem::weakly_canonical(path);
     }
