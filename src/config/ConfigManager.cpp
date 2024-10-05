b72451d1e04ab715e479df9514a108603194149f
The-Emperor10 <44948520+The-Emperor10@users.noreply.github.com>
2024-10-05 16:19:31 -0400

    config: add input-field dots_fade_time option
    * config: add input-field dots_fade_time option
    Time is in milliseconds. Anything <= 0 is immediate.
    
    * input-field: change speedPerSecond to fadeMs
    Default is 200ms, which is the same amount of time as the previous
    5 speedPerSecond. Not sure using doubles is necessary, but I'm
    using them to avoid precision issues.
    
    Closes: https://github.com/hyprwm/hyprlock/issues/355

--- src/config/ConfigManager.cpp
+++ src/config/ConfigManager.cpp
@@ -105,6 +105,7 @@ void CConfigManager::init() {
     m_config.addSpecialConfigValue("input-field", "dots_center", Hyprlang::INT{1});
     m_config.addSpecialConfigValue("input-field", "dots_spacing", Hyprlang::FLOAT{0.2});
     m_config.addSpecialConfigValue("input-field", "dots_rounding", Hyprlang::INT{-1});
+    m_config.addSpecialConfigValue("input-field", "dots_fade_time", Hyprlang::INT{200});
     m_config.addSpecialConfigValue("input-field", "fade_on_empty", Hyprlang::INT{1});
     m_config.addSpecialConfigValue("input-field", "fade_timeout", Hyprlang::INT{2000});
     m_config.addSpecialConfigValue("input-field", "font_color", Hyprlang::INT{0xFF000000});
@@ -258,6 +259,7 @@ std::vector<CConfigManager::SWidgetConfig> CConfigManager::getWidgetConfigs() {
                 {"dots_spacing", m_config.getSpecialConfigValue("input-field", "dots_spacing", k.c_str())},
                 {"dots_center", m_config.getSpecialConfigValue("input-field", "dots_center", k.c_str())},
                 {"dots_rounding", m_config.getSpecialConfigValue("input-field", "dots_rounding", k.c_str())},
+                {"dots_fade_time", m_config.getSpecialConfigValue("input-field", "dots_fade_time", k.c_str())},
                 {"fade_on_empty", m_config.getSpecialConfigValue("input-field", "fade_on_empty", k.c_str())},
                 {"fade_timeout", m_config.getSpecialConfigValue("input-field", "fade_timeout", k.c_str())},
                 {"font_color", m_config.getSpecialConfigValue("input-field", "font_color", k.c_str())},
