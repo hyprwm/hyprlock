91bbba05faf512ec983fc121f77fc50d91178f95
Maximilian Seidler <maximilian.seidler@soundwork.at>
2024-11-02 18:55:18 +0100

    config: add CGradientValueData from Hyprland

--- src/config/ConfigDataValues.hpp
+++ src/config/ConfigDataValues.hpp
@@ -1,12 +1,19 @@
 #pragma once
 #include "../helpers/Log.hpp"
+#include "../helpers/Color.hpp"
 #include <hyprutils/math/Vector2D.hpp>
+#include <hyprutils/string/VarList.hpp>
 #include <any>
 #include <string>
+#include <vector>
+#include <cmath>
+
+using namespace Hyprutils::String;
 
 enum eConfigValueDataTypes {
-    CVD_TYPE_INVALID = -1,
-    CVD_TYPE_LAYOUT  = 0,
+    CVD_TYPE_INVALID  = -1,
+    CVD_TYPE_LAYOUT   = 0,
+    CVD_TYPE_GRADIENT = 1,
 };
 
 class ICustomConfigValueData {
@@ -50,4 +57,58 @@ class CLayoutValueData : public ICustomConfigValueData {
         bool x = false;
         bool y = false;
     } m_sIsRelative;
+};
+
+class CGradientValueData : public ICustomConfigValueData {
+  public:
+    CGradientValueData() {};
+    CGradientValueData(CColor col) {
+        m_vColors.push_back(col);
+    };
+    virtual ~CGradientValueData() {};
+
+    virtual eConfigValueDataTypes getDataType() {
+        return CVD_TYPE_GRADIENT;
+    }
+
+    void reset(CColor col) {
+        m_vColors.clear();
+        m_vColors.emplace_back(col);
+        m_fAngle = 0;
+    }
+
+    /* Vector containing the colors */
+    std::vector<CColor> m_vColors;
+
+    /* Float corresponding to the angle (rad) */
+    float m_fAngle = 0;
+
+    //
+    bool operator==(const CGradientValueData& other) const {
+        if (other.m_vColors.size() != m_vColors.size() || m_fAngle != other.m_fAngle)
+            return false;
+
+        for (size_t i = 0; i < m_vColors.size(); ++i)
+            if (m_vColors[i] != other.m_vColors[i])
+                return false;
+
+        return true;
+    }
+
+    virtual std::string toString() {
+        std::string result;
+        for (auto& c : m_vColors) {
+            result += std::format("{:x} ", c.getAsHex());
+        }
+
+        result += std::format("{}deg", (int)(m_fAngle * 180.0 / M_PI));
+        return result;
+    }
+
+    static CGradientValueData* fromAnyPv(const std::any& v) {
+        RASSERT(v.type() == typeid(void*), "Invalid config value type");
+        const auto P = (CGradientValueData*)std::any_cast<void*>(v);
+        RASSERT(P, "Empty config value");
+        return P;
+    }
 };
--- src/helpers/MiscFunctions.cpp
+++ src/helpers/MiscFunctions.cpp
@@ -1,5 +1,9 @@
 #include <filesystem>
 #include "MiscFunctions.hpp"
+#include "../helpers/Log.hpp"
+#include <hyprutils/string/String.hpp>
+
+using namespace Hyprutils::String;
 
 std::string absolutePath(const std::string& rawpath, const std::string& currentDir) {
     std::filesystem::path path(rawpath);
@@ -15,4 +19,44 @@ std::string absolutePath(const std::string& rawpath, const std::string& currentD
     } else {
         return std::filesystem::weakly_canonical(path);
     }
+}
+
+int64_t configStringToInt(const std::string& VALUE) {
+    if (VALUE.starts_with("0x")) {
+        // Values with 0x are hex
+        const auto VALUEWITHOUTHEX = VALUE.substr(2);
+        return stol(VALUEWITHOUTHEX, nullptr, 16);
+    } else if (VALUE.starts_with("rgba(") && VALUE.ends_with(')')) {
+        const auto VALUEWITHOUTFUNC = VALUE.substr(5, VALUE.length() - 6);
+
+        if (trim(VALUEWITHOUTFUNC).length() != 8) {
+            Debug::log(WARN, "invalid length {} for rgba", VALUEWITHOUTFUNC.length());
+            throw std::invalid_argument("rgba() expects length of 8 characters (4 bytes)");
+        }
+
+        const auto RGBA = std::stol(VALUEWITHOUTFUNC, nullptr, 16);
+
+        // now we need to RGBA -> ARGB. The config holds ARGB only.
+        return (RGBA >> 8) + 0x1000000 * (RGBA & 0xFF);
+    } else if (VALUE.starts_with("rgb(") && VALUE.ends_with(')')) {
+        const auto VALUEWITHOUTFUNC = VALUE.substr(4, VALUE.length() - 5);
+
+        if (trim(VALUEWITHOUTFUNC).length() != 6) {
+            Debug::log(WARN, "invalid length {} for rgb", VALUEWITHOUTFUNC.length());
+            throw std::invalid_argument("rgb() expects length of 6 characters (3 bytes)");
+        }
+
+        const auto RGB = std::stol(VALUEWITHOUTFUNC, nullptr, 16);
+
+        return RGB + 0xFF000000; // 0xFF for opaque
+    } else if (VALUE.starts_with("true") || VALUE.starts_with("on") || VALUE.starts_with("yes")) {
+        return 1;
+    } else if (VALUE.starts_with("false") || VALUE.starts_with("off") || VALUE.starts_with("no")) {
+        return 0;
+    }
+
+    if (VALUE.empty() || !isNumber(VALUE))
+        return 0;
+
+    return std::stoll(VALUE);
 }
