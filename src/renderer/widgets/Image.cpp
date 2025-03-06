hyprwm/hyprlock

commit 9f37c1c8e9923dff65b4b1771694be3be59b1836

core: more hyprutils smart pointer usage and safe references to widgets (#686)
* core: move to UP and make widgets use SPs

* widgets: make widgets have a self ref to avoid UB

* fix shadows and let them have a WP to widgets

src/renderer/widgets/Image.cpp
@@ -8,26 +8,23 @@
 #include <hyprlang.hpp>
 
 CImage::~CImage() {
-    if (imageTimer) {
-        imageTimer->cancel();
-        imageTimer.reset();
+    reset();
+}
+
+void CImage::registerSelf(const SP<CImage>& self) {
+    m_self = self;
+}
+
+static void onTimer(WP<CImage> ref) {
+    if (auto PIMAGE = ref.lock(); PIMAGE) {
+        PIMAGE->onTimerUpdate();
+        PIMAGE->plantTimer();
     }
 }
 
-static void onTimer(std::shared_ptr<CTimer> self, void* data) {
-    const auto PIMAGE = (CImage*)data;
-
-    PIMAGE->onTimerUpdate();
-    PIMAGE->plantTimer();
-}
-
-static void onAssetCallback(void* data) {
-    const auto PIMAGE = (CImage*)data;
-    PIMAGE->renderUpdate();
-}
-
-static void onAssetCallbackTimer(std::shared_ptr<CTimer> self, void* data) {
-    onAssetCallback(data);
+static void onAssetCallback(WP<CImage> ref) {
+    if (auto PIMAGE = ref.lock(); PIMAGE)
+        PIMAGE->renderUpdate();
 }
 
 void CImage::onTimerUpdate() {
@@ -65,9 +62,7 @@ void CImage::onTimerUpdate() {
     pendingResourceID = request.id;
     request.asset     = path;
     request.type      = CAsyncResourceGatherer::eTargetType::TARGET_IMAGE;
-
-    request.callback     = onAssetCallback;
-    request.callbackData = this;
+    request.callback  = [REF = m_self]() { onAssetCallback(REF); };
 
     g_pRenderer->asyncResourceGatherer->requestAsyncAssetPreload(request);
 }
@@ -75,20 +70,24 @@ void CImage::onTimerUpdate() {
 void CImage::plantTimer() {
 
     if (reloadTime == 0) {
-        imageTimer = g_pHyprlock->addTimer(std::chrono::hours(1), onTimer, this, true);
+        imageTimer = g_pHyprlock->addTimer(std::chrono::hours(1), [REF = m_self](auto, auto) { onTimer(REF); }, nullptr, true);
     } else if (reloadTime > 0)
-        imageTimer = g_pHyprlock->addTimer(std::chrono::seconds(reloadTime), onTimer, this, false);
+        imageTimer = g_pHyprlock->addTimer(std::chrono::seconds(reloadTime), [REF = m_self](auto, auto) { onTimer(REF); }, nullptr, false);
 }
 
-CImage::CImage(const Vector2D& viewport_, COutput* output_, const std::string& resourceID_, const std::unordered_map<std::string, std::any>& props) :
-    viewport(viewport_), resourceID(resourceID_), output(output_), shadow(this, props, viewport_) {
+void CImage::configure(const std::unordered_map<std::string, std::any>& props, const SP<COutput>& pOutput) {
+    reset();
+
+    viewport = pOutput->getViewport();
+
+    shadow.configure(m_self.lock(), props, viewport);
 
     try {
         size     = std::any_cast<Hyprlang::INT>(props.at("size"));
         rounding = std::any_cast<Hyprlang::INT>(props.at("rounding"));
         border   = std::any_cast<Hyprlang::INT>(props.at("border_size"));
         color    = *CGradientValueData::fromAnyPv(props.at("border_color"));
-        pos      = CLayoutValueData::fromAnyPv(props.at("position"))->getAbsolute(viewport_);
+        pos      = CLayoutValueData::fromAnyPv(props.at("position"))->getAbsolute(viewport);
         halign   = std::any_cast<Hyprlang::STRING>(props.at("halign"));
         valign   = std::any_cast<Hyprlang::STRING>(props.at("valign"));
         angle    = std::any_cast<Hyprlang::FLOAT>(props.at("rotate"));
@@ -111,6 +110,25 @@ CImage::CImage(const Vector2D& viewport_, COutput* output_, const std::string& r
 
         plantTimer();
     }
+}
+
+void CImage::reset() {
+    if (imageTimer) {
+        imageTimer->cancel();
+        imageTimer.reset();
+    }
+
+    if (g_pHyprlock->m_bTerminate)
+        return;
+
+    imageFB.release();
+
+    if (asset)
+        g_pRenderer->asyncResourceGatherer->unloadAsset(asset);
+
+    asset             = nullptr;
+    pendingResourceID = "";
+    resourceID        = "";
 }
 
 bool CImage::draw(const SRenderData& data) {
@@ -204,8 +222,11 @@ void CImage::renderUpdate() {
         pendingResourceID = "";
     } else if (!pendingResourceID.empty()) {
         Debug::log(WARN, "Asset {} not available after the asyncResourceGatherer's callback!", pendingResourceID);
+        pendingResourceID = "";
+    } else if (!pendingResourceID.empty()) {
+        Debug::log(WARN, "Asset {} not available after the asyncResourceGatherer's callback!", pendingResourceID);
 
-        g_pHyprlock->addTimer(std::chrono::milliseconds(100), onAssetCallbackTimer, this);
+        g_pHyprlock->addTimer(std::chrono::milliseconds(100), [REF = m_self](auto, auto) { onAssetCallback(REF); }, nullptr);
     }
 
     g_pHyprlock->renderOutput(output->stringPort);

src/auth/Auth.cpp
@@ -11,10 +11,10 @@
 CAuth::CAuth() {
     static const auto ENABLEPAM = g_pConfigManager->getValue<Hyprlang::INT>("auth:pam:enabled");
     if (*ENABLEPAM)
-        m_vImpls.push_back(std::make_shared<CPam>());
+        m_vImpls.emplace_back(makeShared<CPam>());
     static const auto ENABLEFINGERPRINT = g_pConfigManager->getValue<Hyprlang::INT>("auth:fingerprint:enabled");
     if (*ENABLEFINGERPRINT)
-        m_vImpls.push_back(std::make_shared<CFingerprint>());
+        m_vImpls.emplace_back(makeShared<CFingerprint>());
 
     RASSERT(!m_vImpls.empty(), "At least one authentication method must be enabled!");
 }
@@ -32,12 +32,7 @@ void CAuth::submitInput(const std::string& input) {
 }
 
 bool CAuth::checkWaiting() {
-    for (const auto& i : m_vImpls) {
-        if (i->checkWaiting())
-            return true;
-    }
-
-    return false;
+    return std::ranges::any_of(m_vImpls, [](const auto& i) { return i->checkWaiting(); });
 }
 
 const std::string& CAuth::getCurrentFailText() {
@@ -64,7 +59,7 @@ size_t CAuth::getFailedAttempts() {
     return m_sCurrentFail.failedAttempts;
 }
 
-std::shared_ptr<IAuthImplementation> CAuth::getImpl(eAuthImplementations implType) {
+SP<IAuthImplementation> CAuth::getImpl(eAuthImplementations implType) {
     for (const auto& i : m_vImpls) {
         if (i->getImplType() == implType)
             return i;

src/auth/Auth.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <memory>
 #include <optional>
 #include <vector>
+
+#include "../defines.hpp"
 
 enum eAuthImplementations {
     AUTH_IMPL_PAM         = 0,
@@ -28,23 +29,23 @@ class CAuth {
   public:
     CAuth();
 
-    void                                 start();
+    void                       start();
 
-    void                                 submitInput(const std::string& input);
-    bool                                 checkWaiting();
+    void                       submitInput(const std::string& input);
+    bool                       checkWaiting();
 
-    const std::string&                   getCurrentFailText();
+    const std::string&         getCurrentFailText();
 
-    std::optional<std::string>           getFailText(eAuthImplementations implType);
-    std::optional<std::string>           getPrompt(eAuthImplementations implType);
-    size_t                               getFailedAttempts();
+    std::optional<std::string> getFailText(eAuthImplementations implType);
+    std::optional<std::string> getPrompt(eAuthImplementations implType);
+    size_t                     getFailedAttempts();
 
-    std::shared_ptr<IAuthImplementation> getImpl(eAuthImplementations implType);
+    SP<IAuthImplementation>    getImpl(eAuthImplementations implType);
 
-    void                                 terminate();
+    void                       terminate();
 
-    void                                 enqueueUnlock();
-    void                                 enqueueFail(const std::string& failText, eAuthImplementations implType);
+    void                       enqueueUnlock();
+    void                       enqueueFail(const std::string& failText, eAuthImplementations implType);
 
     // Should only be set via the main thread
     bool m_bDisplayFailText = false;
@@ -56,7 +57,7 @@ class CAuth {
         size_t               failedAttempts = 0;
     } m_sCurrentFail;
 
-    std::vector<std::shared_ptr<IAuthImplementation>> m_vImpls;
+    std::vector<SP<IAuthImplementation>> m_vImpls;
 };
 
-inline std::unique_ptr<CAuth> g_pAuth;
+inline UP<CAuth> g_pAuth;

src/config/ConfigDataValues.hpp
@@ -27,7 +27,7 @@ class ICustomConfigValueData {
 
 class CLayoutValueData : public ICustomConfigValueData {
   public:
-    CLayoutValueData() {};
+    CLayoutValueData() = default;
     virtual ~CLayoutValueData() {};
 
     virtual eConfigValueDataTypes getDataType() {

src/config/ConfigManager.hpp
@@ -5,7 +5,6 @@
 #include <hyprlang.hpp>
 #include <optional>
 #include <vector>
-#include <memory>
 #include <unordered_map>
 
 #include "../defines.hpp"
@@ -41,4 +40,4 @@ class CConfigManager {
     Hyprlang::CConfig m_config;
 };
 
-inline std::unique_ptr<CConfigManager> g_pConfigManager;
+inline UP<CConfigManager> g_pConfigManager;

src/core/AnimationManager.hpp
@@ -4,7 +4,6 @@
 #include <hyprutils/animation/AnimatedVariable.hpp>
 
 #include "../helpers/AnimatedVariable.hpp"
-#include "../helpers/Math.hpp"
 #include "../defines.hpp"
 
 class CHyprlockAnimationManager : public Hyprutils::Animation::CAnimationManager {
@@ -31,4 +30,4 @@ class CHyprlockAnimationManager : public Hyprutils::Animation::CAnimationManager
     bool m_bTickScheduled = false;
 };
 
-inline std::unique_ptr<CHyprlockAnimationManager> g_pAnimationManager;
+inline UP<CHyprlockAnimationManager> g_pAnimationManager;

src/core/Egl.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <memory>
-
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
+
+#include "../defines.hpp"
 
 class CEGL {
   public:
@@ -19,4 +19,4 @@ class CEGL {
     void                                     makeCurrent(EGLSurface surf);
 };
 
-inline std::unique_ptr<CEGL> g_pEGL;
+inline UP<CEGL> g_pEGL;

src/core/LockSurface.cpp
@@ -11,13 +11,9 @@ CSessionLockSurface::~CSessionLockSurface() {
         wl_egl_window_destroy(eglWindow);
 }
 
-CSessionLockSurface::CSessionLockSurface(COutput* output) : output(output) {
+CSessionLockSurface::CSessionLockSurface(const SP<COutput>& pOutput) : m_outputRef(pOutput), m_outputID(pOutput->m_ID) {
     surface = makeShared<CCWlSurface>(g_pHyprlock->getCompositor()->sendCreateSurface());
-
-    if (!surface) {
-        Debug::log(CRIT, "Couldn't create wl_surface");
-        exit(1);
-    }
+    RASSERT(surface, "Couldn't create wl_surface");
 
     static const auto FRACTIONALSCALING = g_pConfigManager->getValue<Hyprlang::INT>("general:fractional_scaling");
     const auto        ENABLE_FSV1       = *FRACTIONALSCALING == 1 || /* auto enable */ (*FRACTIONALSCALING == 2);
@@ -45,7 +41,7 @@ CSessionLockSurface::CSessionLockSurface(COutput* output) : output(output) {
     if (!PVIEWPORTER)
         Debug::log(LOG, "No viewporter support! Oops, won't be able to scale!");
 
-    lockSurface = makeShared<CCExtSessionLockSurfaceV1>(g_pHyprlock->getSessionLock()->sendGetLockSurface(surface->resource(), output->output->resource()));
+    lockSurface = makeShared<CCExtSessionLockSurfaceV1>(g_pHyprlock->getSessionLock()->sendGetLockSurface(surface->resource(), pOutput->m_wlOutput->resource()));
 
     if (!lockSurface) {
         Debug::log(CRIT, "Couldn't create ext_session_lock_surface_v1");
@@ -62,6 +58,8 @@ void CSessionLockSurface::configure(const Vector2D& size_, uint32_t serial_) {
     const bool SAMESIZE   = logicalSize == size_;
     const bool SAMESCALE  = appliedScale == fractionalScale;
 
+    const auto POUTPUT = m_outputRef.lock();
+
     serial       = serial_;
     logicalSize  = size_;
     appliedScale = fractionalScale;
@@ -71,8 +69,8 @@ void CSessionLockSurface::configure(const Vector2D& size_, uint32_t serial_) {
         viewport->sendSetDestination(logicalSize.x, logicalSize.y);
         surface->sendSetBufferScale(1);
     } else {
-        size = size_ * output->scale;
-        surface->sendSetBufferScale(output->scale);
+        size = size_ * POUTPUT->scale;
+        surface->sendSetBufferScale(POUTPUT->scale);
     }
 
     if (!SAMESERIAL)
@@ -103,8 +101,8 @@ void CSessionLockSurface::configure(const Vector2D& size_, uint32_t serial_) {
     }
 
     if (readyForFrame && !(SAMESIZE && SAMESCALE)) {
-        g_pRenderer->removeWidgetsFor(this);
-        Debug::log(LOG, "Reloading widgets");
+        Debug::log(LOG, "output {} changed, reloading widgets!", POUTPUT->stringPort);
+        g_pRenderer->reconfigureWidgetsFor(POUTPUT->m_ID);
     }
 
     readyForFrame = true;
@@ -129,7 +127,10 @@ void CSessionLockSurface::render() {
         if (g_pHyprlock->m_bTerminate)
             return;
 
-        Debug::log(TRACE, "[{}] frame {}, Current fps: {:.2f}", output->stringPort, m_frames, 1000.f / (frameTime - m_lastFrameTime));
+        if (Debug::verbose) {
+            const auto POUTPUT = m_outputRef.lock();
+            Debug::log(TRACE, "[{}] frame {}, Current fps: {:.2f}", POUTPUT->stringPort, m_frames, 1000.f / (frameTime - m_lastFrameTime));
+        }
 
         m_lastFrameTime = frameTime;
 

commit ba147d66eef2363e170012fe5c82a10629624e39

image: set resourceId in configure
Fixes a regression caused by #686 (Images don't render cause of a missing resourceId)

src/renderer/widgets/Image.cpp
@@ -101,7 +101,8 @@ void CImage::configure(const std::unordered_map<std::string, std::any>& props, c
         RASSERT(false, "Missing propperty for CImage: {}", e.what()); //
     }
 
-    angle = angle * M_PI / 180.0;
+    resourceID = "image:" + path;
+    angle      = angle * M_PI / 180.0;
 
     if (reloadTime > -1) {
         try {
@@ -123,7 +124,7 @@ void CImage::reset() {
 
     imageFB.release();
 
-    if (asset)
+    if (asset && reloadTime > -1) // Don't unload asset if it's a static image
         g_pRenderer->asyncResourceGatherer->unloadAsset(asset);
 
     asset             = nullptr;