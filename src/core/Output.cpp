6d2d4133e9766e912fd551f1f1504bed940119f9
Maximilian Seidler <maximilian.seidler@soundwork.at>
2025-04-04 09:43:07 +0200

    output: refuse to create session lock surfaces with size 0x0

--- src/core/Output.cpp
+++ src/core/Output.cpp
@@ -56,6 +56,11 @@ void COutput::createSessionLockSurface() {
         return;
     }
 
+    if (size == Vector2D{0, 0}) {
+        Debug::log(WARN, "output {} refusing to create a lock surface with size 0x0", m_ID);
+        return;
+    }
+
     m_sessionLockSurface = makeUnique<CSessionLockSurface>(m_self.lock());
 }
 
