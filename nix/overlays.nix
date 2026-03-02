38a314298b87fda283e921aa6280be414f95ae4c
Mihai Fufezan <mihai@fufexan.net>
2026-03-02 18:30:05 +0200

    treewide: alejandra -> nixfmt

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -2,15 +2,19 @@
   lib,
   inputs,
   self,
-}: let
-  mkDate = longDate: (lib.concatStringsSep "-" [
-    (builtins.substring 0 4 longDate)
-    (builtins.substring 4 2 longDate)
-    (builtins.substring 6 2 longDate)
-  ]);
+}:
+let
+  mkDate =
+    longDate:
+    (lib.concatStringsSep "-" [
+      (builtins.substring 0 4 longDate)
+      (builtins.substring 4 2 longDate)
+      (builtins.substring 6 2 longDate)
+    ]);
 
   version = lib.removeSuffix "\n" (builtins.readFile ../VERSION);
-in {
+in
+{
   default = inputs.self.overlays.hyprlock;
 
   hyprlock = lib.composeManyExtensions [
@@ -21,7 +25,12 @@ in {
     (final: prev: {
       hyprlock = prev.callPackage ./default.nix {
         stdenv = prev.gcc15Stdenv;
-        version = version + "+date=" + (mkDate (inputs.self.lastModifiedDate or "19700101")) + "_" + (inputs.self.shortRev or "dirty");
+        version =
+          version
+          + "+date="
+          + (mkDate (inputs.self.lastModifiedDate or "19700101"))
+          + "_"
+          + (inputs.self.shortRev or "dirty");
         inherit (final) hyprlang;
         shortRev = self.sourceInfo.shortRev or "dirty";
       };
