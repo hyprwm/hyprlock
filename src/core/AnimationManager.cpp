84aa55d2235b10b80d6e2c2c6f3fd2dd53b28777
Maximilian Seidler <maximilian.seidler@soundwork.at>
2026-04-17 10:34:59 +0200

    core: eglReleaseThread when we have no outputs
    This fixes monitor on/off on the somewhat broken nvidia 580 drivers
    and potentially other nvidia drivers as well.

--- src/core/AnimationManager.cpp
+++ src/core/AnimationManager.cpp
@@ -78,7 +78,7 @@ void updateGradientVariable(CAnimatedVariable<CGradientValueData>& av, const flo
 
 void CHyprlockAnimationManager::tick() {
     static const auto ANIMATIONSENABLED = g_pConfigManager->getValue<Hyprlang::INT>("animations:enabled");
-    const auto        CPY = m_vActiveAnimatedVariables;
+    const auto        CPY               = m_vActiveAnimatedVariables;
 
     for (const auto& PAV : CPY) {
         if (!PAV || !PAV->ok())
