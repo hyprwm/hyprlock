hyprwm/hyprlock

commit e1dca578ed58fa370e3b05ff068daee65640732a

input-field: add `check_text` option (#959)

src/renderer/widgets/PasswordInputField.cpp
@@ -50,6 +50,7 @@ void CPasswordInputField::configure(const std::unordered_map<std::string, std::a
         rounding                 = std::any_cast<Hyprlang::INT>(props.at("rounding"));
         configPlaceholderText    = std::any_cast<Hyprlang::STRING>(props.at("placeholder_text"));
         configFailText           = std::any_cast<Hyprlang::STRING>(props.at("fail_text"));
+        configCheckText          = std::any_cast<Hyprlang::STRING>(props.at("check_text"));
         fontFamily               = std::any_cast<Hyprlang::STRING>(props.at("font_family"));
         colorConfig.outer        = CGradientValueData::fromAnyPv(props.at("outer_color"));
         colorConfig.inner        = std::any_cast<Hyprlang::INT>(props.at("inner_color"));
@@ -163,7 +164,7 @@ void CPasswordInputField::updateDots() {
     if (dots.currentAmount->goal() == passwordLength)
         return;
 
-    if (checkWaiting)
+    if (checkWaiting && configCheckText.empty())
         return;
 
     if (passwordLength == 0)
@@ -299,7 +300,9 @@ bool CPasswordInputField::draw(const SRenderData& data) {
         }
     }
 
-    if (passwordLength == 0 && !checkWaiting && placeholder.resourceID > 0) {
+    bool placeholderPasswordCondition = (passwordLength == 0 && placeholder.resourceID > 0);
+
+    if (placeholderPasswordCondition && (!checkWaiting || (checkWaiting && !configCheckText.empty()))) {
         ASP<CTexture> currAsset = nullptr;
 
         if (!placeholder.asset)
@@ -325,6 +328,7 @@ bool CPasswordInputField::draw(const SRenderData& data) {
 }
 
 void CPasswordInputField::updatePlaceholder() {
+
     if (passwordLength != 0) {
         if (placeholder.asset && /* keep prompt asset cause it is likely to be used again */ displayFail) {
             g_asyncResourceManager->unload(placeholder.asset);
@@ -339,9 +343,16 @@ void CPasswordInputField::updatePlaceholder() {
     if (displayFail && placeholder.failedAttempts == g_pAuth->getFailedAttempts())
         return;
 
-    placeholder.failedAttempts = g_pAuth->getFailedAttempts();
+    std::string& templateText = configPlaceholderText;
 
-    std::string newText = (displayFail) ? formatString(configFailText).formatted : formatString(configPlaceholderText).formatted;
+    if (displayFail) {
+        templateText               = configFailText;
+        placeholder.failedAttempts = g_pAuth->getFailedAttempts();
+    } else if (checkWaiting && !configCheckText.empty()) {
+        templateText = configCheckText;
+    }
+
+    const std::string newText = formatString(templateText).formatted;
 
     // if the text is unchanged we don't need to do anything, unless we are swapping font color
     const auto ALLOWCOLORSWAP = outThick == 0 && colorConfig.swapFont;
@@ -435,7 +446,12 @@ void CPasswordInputField::updateColors() {
 
     CGradientValueData* outerTarget = colorConfig.outer;
     CHyprColor          innerTarget = colorConfig.inner;
-    CHyprColor          fontTarget  = (displayFail) ? colorConfig.fail->m_vColors.front() : colorConfig.font;
+    CHyprColor          fontTarget  = colorConfig.font;
+
+    if (displayFail)
+        fontTarget = colorConfig.fail->m_vColors.front();
+    else if (checkWaiting)
+        fontTarget = configCheckText.empty() ? colorConfig.font : colorConfig.check->m_vColors.front();
 
     if (targetGrad) {
         if (BORDERLESS && colorConfig.swapFont) {

assets/example.conf
@@ -57,6 +57,9 @@ input-field {
     placeholder_text = Input password...
     fail_text = $PAMFAIL
 
+    # uncomment if you wish to display a message during authentication
+    # check_text = Authenticating...
+
     # uncomment to use a letter instead of a dot to indicate the typed password
     # dots_text_format = *
     # dots_size = 0.4

src/config/ConfigManager.cpp
@@ -303,6 +303,7 @@ void CConfigManager::init() {
     m_config.addSpecialConfigValue("input-field", "check_color", GRADIENTCONFIG("0xFF22CC88"));
     m_config.addSpecialConfigValue("input-field", "fail_color", GRADIENTCONFIG("0xFFCC2222"));
     m_config.addSpecialConfigValue("input-field", "fail_text", Hyprlang::STRING{"<i>$FAIL</i>"});
+    m_config.addSpecialConfigValue("input-field", "check_text", Hyprlang::STRING{""});
     m_config.addSpecialConfigValue("input-field", "capslock_color", GRADIENTCONFIG(""));
     m_config.addSpecialConfigValue("input-field", "numlock_color", GRADIENTCONFIG(""));
     m_config.addSpecialConfigValue("input-field", "bothlock_color", GRADIENTCONFIG(""));
@@ -484,6 +485,7 @@ std::vector<CConfigManager::SWidgetConfig> CConfigManager::getWidgetConfigs() {
                 {"check_color", m_config.getSpecialConfigValue("input-field", "check_color", k.c_str())},
                 {"fail_color", m_config.getSpecialConfigValue("input-field", "fail_color", k.c_str())},
                 {"fail_text", m_config.getSpecialConfigValue("input-field", "fail_text", k.c_str())},
+                {"check_text", m_config.getSpecialConfigValue("input-field", "check_text", k.c_str())},
                 {"capslock_color", m_config.getSpecialConfigValue("input-field", "capslock_color", k.c_str())},
                 {"numlock_color", m_config.getSpecialConfigValue("input-field", "numlock_color", k.c_str())},
                 {"bothlock_color", m_config.getSpecialConfigValue("input-field", "bothlock_color", k.c_str())},

src/renderer/widgets/PasswordInputField.hpp
@@ -56,7 +56,7 @@ class CPasswordInputField : public IWidget {
     Vector2D                 configPos;
     Vector2D                 configSize;
 
-    std::string              halign, valign, configFailText, outputStringPort, configPlaceholderText, fontFamily;
+    std::string              halign, valign, configFailText, configCheckText, outputStringPort, configPlaceholderText, fontFamily;
     uint64_t                 configFailTimeoutMs = 2000;
 
     int                      outThick, rounding;

commit 20d5b7ca6da760afca1b400642e07d112f31df4d

input-field: fix placeholder not updating correctly
Bug introduced in #959, reference made the config text be overwritten

src/renderer/widgets/PasswordInputField.cpp
@@ -343,16 +343,15 @@ void CPasswordInputField::updatePlaceholder() {
     if (displayFail && placeholder.failedAttempts == g_pAuth->getFailedAttempts())
         return;
 
-    std::string& templateText = configPlaceholderText;
+    std::string newText = "";
 
     if (displayFail) {
-        templateText               = configFailText;
+        newText               = formatString(configFailText).formatted;
         placeholder.failedAttempts = g_pAuth->getFailedAttempts();
-    } else if (checkWaiting && !configCheckText.empty()) {
-        templateText = configCheckText;
-    }
-
-    const std::string newText = formatString(templateText).formatted;
+    } else if (checkWaiting && !configCheckText.empty())
+        newText = formatString(configCheckText).formatted;
+    else
+        newText = formatString(configPlaceholderText).formatted;
 
     // if the text is unchanged we don't need to do anything, unless we are swapping font color
     const auto ALLOWCOLORSWAP = outThick == 0 && colorConfig.swapFont;